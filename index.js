507e9198a5e127a469fde8b9cb808b02ae4bd329
Dazaa1 <marouanelankaoui066@gmail.com>
2025-06-08 21:25:59 +0100

    build a simple version just serving a simple html line through an endpoint.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-import { Server } from 'socket.io';
+const express  = require('express');
+const { createServer } = require('node:http');
 
+// initialse express app
+const app = express();
+const server = createServer(app);
+
+// first endpoint which is route / serving some html
+app.get('/', (req, res) => {
+    res.send('<h1> Hello world</h1>');
+})
+
+// listening to port 3000
+server.listen(3000, () => {
+    console.log('Server running at https://localhost:3000')
+})
