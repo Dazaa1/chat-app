86881965add257070f7f32ef6d81aea6905a8682
Dazaa1 <marouanelankaoui066@gmail.com>
2025-06-08 21:28:55 +0100

    change to make it server and html document.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express  = require('express');
 const { createServer } = require('node:http');
+const { join } = require('node:path');
 
 // initialse express app
 const app = express();
@@ -7,7 +8,7 @@ const server = createServer(app);
 
 // first endpoint which is route / serving some html
 app.get('/', (req, res) => {
-    res.send('<h1> Hello world</h1>');
+    res.sendFile(join(__dirname, 'index.html'));
 })
 
 // listening to port 3000
