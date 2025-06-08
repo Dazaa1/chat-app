590f7ce774a3efdbb4dbd6a9da2b455f6b82d185
Dazaa1 <marouanelankaoui066@gmail.com>
2025-06-08 21:35:55 +0100

    integrate socket.io.

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express  = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
+const { Server } = require('socket.io');
 
 // initialse express app
 const app = express();
 const server = createServer(app);
+// new instance of socket.io
+const io = new Server(server);
 
 // first endpoint which is route / serving some html
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
+})
+
+// listening on the connection event for upcoming connections.
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 })
 
 // listening to port 3000
