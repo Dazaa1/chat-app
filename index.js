470c3e083f3f6c521c816374abd7adbce5b58873
Dazaa1 <marouanelankaoui066@gmail.com>
2025-06-09 11:33:14 +0100

    Fix port.

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ io.on('connection', (socket) => {
     });
 });
 
-
+const PORT = process.env.PORT || 3000;
 // listening to port 3000
-server.listen(3000, () => {
-    console.log('Server running at https://localhost:3000')
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 })
